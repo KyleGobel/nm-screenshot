9eedb6602b20abbf6afcad829c30a807ef906426
Kyle <kgobel@gmail.com>
2015-01-11 03:42:06 -0500

    access-control-allow-headers doesn't allow wildcards

--- src/appHost.js
+++ src/appHost.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 app.use(function(req, res, next) {
 	res.setHeader('Access-Control-Allow-Origin', '*');
 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
-	res.setHeader('Access-Control-Allow-Headers', '*');
+	res.setHeader('Access-Control-Allow-Headers', req.getHeader("Access-Control-Request-Headers"));
 
 	next();
 });
