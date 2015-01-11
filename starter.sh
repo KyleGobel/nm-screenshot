3bd14f26b3c67ff957c7f03adfb7736c46ecf0a4
Kyle <kgobel@gmail.com>
2015-01-11 03:56:25 -0500

    added sudo to starter.sh

--- starter.sh
+++ starter.sh
@@ -3,5 +3,5 @@
 if [ $(ps -e -o uid,cmd | grep $UID | grep node | grep -v grep | wc -l | tr -s "\n") -eq 0 ]
 then
         export PATH=/usr/local/bin:$PATH
-        forever start --sourceDir /home/ubuntu/nm-screenshot src/appHost.js >> /home/ubuntu/nm-screenshot.log 2>&1
+        sudo forever start --sourceDir /home/ubuntu/nm-screenshot src/appHost.js >> /home/ubuntu/nm-screenshot.log 2>&1
 fi
